15d8737cd9864d87b436344ccbc3633c507a599b
sijiyanci <1056772907@qq.com>
2020-04-23 13:13:46 +0800

    test3

---
+++ src/test/start.java
@@ -0,0 +1,13 @@
+package test;
+import javax.swing.*;
+import java.awt.*;
+
+public class start {
+    public static void main(String[] args){
+        JFrame frame=new JFrame("the first");
+        frame.setSize(500,1000);
+        frame.getContentPane().setBackground(Color.red);
+        frame.setVisible(true);
+    }
+}
+
