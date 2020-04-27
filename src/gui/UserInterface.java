6d4138969364a8026def37b7ed7890f81710b9cf
yisan1113 <1271914094@qq.com>
2020-04-27 19:28:46 +0800

    包合并

--- src/gui/UserInterface.java
+++ src/gui/UserInterface.java
@@ -1,4 +1,4 @@
-package GUI;
+package gui;
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
--- src/gui/registerGUI.java
+++ src/gui/registerGUI.java
@@ -1,4 +1,4 @@
-package GUI;
+package gui;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
